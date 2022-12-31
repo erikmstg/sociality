5bfe67494e67d51582947b7b9d32a11824b5429d
Erik Mikael <erikfreecss@gmail.com>
2022-12-31 12:32:10 +0700

    updated

--- controllers/users.js
+++ controllers/users.js
@@ -17,9 +17,11 @@ export const getUserFriends = async (req, res) => {
     const user = await User.findById(id);
 
     // grab all information from user.friends
-    const friends = await Promise.all(user.friends((id) => User.findById(id)));
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
 
-    const formattedFriends = friends?.map(
+    const formattedFriends = friends.map(
       ({ _id, firstName, lastName, occupation, location, picturePath }) => {
         // return the object that grabbed
         return { _id, firstName, lastName, occupation, location, picturePath };
